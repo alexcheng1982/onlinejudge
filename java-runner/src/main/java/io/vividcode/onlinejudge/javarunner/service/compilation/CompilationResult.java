46e8d77bba7efa218b5123ae6acd4c4a828b5f10
Fu Cheng <alexcheng1982@gmail.com>
2024-01-10 08:37:15 +0800

    Update check for errors

--- java-runner/src/main/java/io/vividcode/onlinejudge/javarunner/service/compilation/CompilationResult.java
+++ java-runner/src/main/java/io/vividcode/onlinejudge/javarunner/service/compilation/CompilationResult.java
@@ -1,10 +1,11 @@
 package io.vividcode.onlinejudge.javarunner.service.compilation;
 
 import java.util.List;
+import org.springframework.util.CollectionUtils;
 
 public record CompilationResult(String packageName, List<CompilationError> errors) {
 
   public boolean success() {
-    return errors.isEmpty();
+    return CollectionUtils.isEmpty(errors);
   }
 }
